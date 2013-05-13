64910899c3530798184f4cebb53c71d4f093bb88
Alex Dergachev <alex@evolvingweb.ca>
2013-05-13 11:24:44 -0400

    Trying jquery.powertip.js instead of jquery.jGrowl.js; first attempt

--- bing-translator-bookmarklet.js
+++ bing-translator-bookmarklet.js
@@ -13,12 +13,13 @@
 
 function requireDeps() {
   yepnope([{
-      test: typeof(window.jQuery) === 'undefined' || jQuery.fn.jquery.match(/^1\.[0-9]+/) <= 1.4,
-      yep: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.4.4/jquery.min.js'
+      test: typeof(window.jQuery) === 'undefined' || jQuery.fn.jquery.match(/^1\.[0-9]+/) < 1.7,
+      yep: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js'
     },
     {
-      test: typeof(window.jQuery) === 'undefined' || typeof(window.jQuery.jGrowl) === 'undefined',
-      yep: ['//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.2.12/jquery.jgrowl.min.js','//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.2.12/jquery.jgrowl.css'],
+      test: typeof(window.jQuery) === 'undefined' || typeof(window.jQuery.fn.powerTip) === 'undefined',
+      //note: 1.2 version isnt on cdnjs.org yet; using gh-pages is good for no-sniff header, but this will not work on HTTPS
+      yep: ['http://stevenbenner.github.io/jquery-powertip/scripts/jquery.powertip.js','http://stevenbenner.github.io/jquery-powertip/styles/jquery.powertip.css'],
       complete: function (url, result, key) {
         initMyBookmarklet(jQuery);
       }
@@ -27,66 +28,41 @@ function requireDeps() {
 }
 
 function initMyBookmarklet($) {
-  $(function(){ 
-      $.jGrowl('Highlight a phrase to translate it.', {header:"FR>EN bookmarklet activated"});
+  // call with no args to clear, o
+  // call with just _word_ to indicate we're looking up word
+  function powerTip(word,answer,event) {
+   $.powerTip.destroy();
+    if (typeof(word) === "undefined" || word == "") {
+      $(event.target).data('powertipjq', '');
+    } else if (typeof(answer) === "undefined") {
+      $(event.target).data('powertipjq', $('<p>Looking up <b>' + word + '</b>...</p>')).powerTip({followMouse:true});
+    } else {
+      $(event.target).data('powertipjq', $('<p><b>' + word + '</b></p>' + "\n" + '<p>' + answer + '</p>')).powerTip({followMouse:true});
+    }
+  }
+
+  $(function() {
+      // $.jGrowl('Highlight a phrase to translate it.', {header:"FR>EN bookmarklet activated"});
       $(document).mouseup(function(e) {
-          var word = window.getSelection().toString();
-          lookupTranslation(word);
+        var word = window.getSelection().toString();
+        powerTip(word, undefined, e);
+        lookupTranslation(word, e);
       });
   });
 
-  function lookupTranslation(word) { 
+  function lookupTranslation(word, event) {
     // ensure selection is a single word, recognizing accents too
     if (word.length < 2) {
-      return;      
+      return;
     }
-
     // there seems to be no other way to escape quotes in YQL syntax
     word = word.replace(/"/,"'");
-
-    var query ='select * from microsoft.translator where text="' + word + '" and from = "fr" and to = "en" and client_id="dictionary-bookmarklet-01" and client_secret="12345678901234567890"';
-    $.get("http://query.yahooapis.com/v1/public/yql", { q: query, env: "store://datatables.org/alltableswithkeys" }, function(data) {
+    var query = 'SELECT * from microsoft.translator WHERE text="' + word + '" AND from = "fr" AND to = "en" AND ' +
+                 'client_id="dictionary-bookmarklet-01" AND client_secret="12345678901234567890"';
+    $.get("http://query.yahooapis.com/v1/public/yql",
+          { q: query, env: "store://datatables.org/alltableswithkeys" }, function(data) {
       var answer = $('query results',data).text();
-      $.jGrowl(answer, {header: word, life: Math.max(word.length * 50, 3000) });
-    });   
-  }
-
-  function lookupDef(word) {
-    // ensure selection is a single word, recognizing accents too
-    word = word.toLowerCase().split(' ')[0].replace(/[^'a-zA-Z0-9\u00E0-\u00FC-]/,'');
-    if (word.length < 2) {
-      return;      
-    }
-
-   $.getJSON(url, function(data) {
-      var items = [];
-      $.each(data.tuc, function(key, val) {    
-        if ( typeof(val) !== 'undefined' && typeof(val.phrase) !== 'undefined' && typeof(val.phrase.text) !== 'undefined') {
-          items.push('<li>' + val.phrase.text + '</li>');         
-        }    
-      });
-      if (items.length == 0) {
-        $.each(data.tuc, function(key, val) {    
-          if (typeof(val) !== 'undefined' && typeof(val.meanings) !== 'undefined') {
-            $.each(val.meanings, function(meaningKey, meaningValue) {
-              if (typeof(meaningValue.text) != 'undefined') {
-                items.push('<li>' + meaningValue.text + '</li>');
-              }
-            });
-          }
-        });
-      }
-      if (items.length == 0) {
-        items.push('<li>No definitions found.</li>');
-      }
-      var list = $('<ul/>', {
-        'class': 'my-new-list',
-        html: items.join('')
-      });
-             
-      
-      $.jGrowl('<b>' + word + '</b>' + list.html());
-              
-    });   
+      powerTip(word,answer,event);
+    });
   }
 }
